6002c411d269bb69b4281d58cf529d2a056d2490
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-12-26 22:24:50 +0000

    solved and tested

--- TreeIntSet.java
+++ TreeIntSet.java
@@ -50,22 +50,20 @@ public class TreeIntSet implements IntSet {
 
 	public boolean containsVerbose(int x) {
 		boolean contains = false;
+		System.out.println(this.value);
 		if (this.value == x) {
-			System.out.println(this.value + ".");
 			contains = true;
 		} else if (x < this.value) {
 			if (this.left == null) {
 				contains = false;
 			} else {
-				System.out.print(this.left + ", ");
-				contains = this.left.contains(x);
+				contains = this.left.containsVerbose(x);
 			}
 		} else {
 			if (this.right == null) {
 				contains = false;
 			} else {
-				System.out.print(this.right + ", ");
-				contains = this.right.contains(x);
+				contains = this.right.containsVerbose(x);
 			}
 		}
 		return contains;
@@ -74,11 +72,32 @@ public class TreeIntSet implements IntSet {
 	public String toString() {
 		String result = this.value + ", ";
 		if (this.left != null) {
-			this.left.toString();
-		} else if (this.right != null) {
-			this.right.toString();
+			result = result + this.left.toString();
+		}
+		if (this.right != null) {
+			result = result + this.right.toString();
 		}
 		return result;
+	}
+
+	public static void main(String[] args) {
+		IntSet start = new TreeIntSet(100);
+		start.add(150);
+		start.add(190);
+		start.add(200);
+		start.add(1);
+		start.add(4);
+		start.add(7);
+		start.add(150);
+		start.add(95);
+		start.add(66);
+		if (start.contains(19)) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+		boolean y = start.containsVerbose(1000);
+		System.out.println(start.toString());
 	}
 
 }
